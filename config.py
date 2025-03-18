3e721958e9584c970dbb612e1c756f1f1ffd7f0b
onemidia <onemidiaindoora@gmail.com>
2025-03-18 05:52:52 -0300

    Initial commit

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-OPENWEATHER_API_KEY = "sua_chave_aqui"
+OPENWEATHER_API_KEY = "9a0dec7f2d04b94b78a7db7f1193fc51"
 CITY = "Taquaritinga"
 COUNTRY = "BR"
 XIBO_ICON_URL = "https://seu-xibo.com/path_dos_icones"
